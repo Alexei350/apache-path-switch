ba26a6eb68acbeea81bd716a7fb7787042d546c6
Alexei <alexeisecretti@hotmail.com>
2019-05-23 15:41:18 -0300

    Commit inicial do projeto

---
+++ ApachePathSwitch/ProjectSelector.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ApachePathSwitch
+{
+    partial class ProjectSelector
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProjectSelector));
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.btnProjetos = new System.Windows.Forms.Button();
+            this.txtProjetos = new System.Windows.Forms.TextBox();
+            this.cmbProjetos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnXampp = new System.Windows.Forms.Button();
+            this.txtXampp = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Enabled = false;
+            this.btnSalvar.Location = new System.Drawing.Point(290, 119);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // btnProjetos
+            // 
+            this.btnProjetos.Location = new System.Drawing.Point(11, 42);
+            this.btnProjetos.Name = "btnProjetos";
+            this.btnProjetos.Size = new System.Drawing.Size(106, 23);
+            this.btnProjetos.TabIndex = 1;
+            this.btnProjetos.Text = "Localizar Projetos";
+            this.btnProjetos.UseVisualStyleBackColor = true;
+            this.btnProjetos.Click += new System.EventHandler(this.BtnProjetos_Click);
+            // 
+            // txtProjetos
+            // 
+            this.txtProjetos.Enabled = false;
+            this.txtProjetos.Location = new System.Drawing.Point(123, 45);
+            this.txtProjetos.Name = "txtProjetos";
+            this.txtProjetos.Size = new System.Drawing.Size(230, 20);
+            this.txtProjetos.TabIndex = 2;
+            // 
+            // cmbProjetos
+            // 
+            this.cmbProjetos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProjetos.FormattingEnabled = true;
+            this.cmbProjetos.Location = new System.Drawing.Point(135, 92);
+            this.cmbProjetos.Name = "cmbProjetos";
+            this.cmbProjetos.Size = new System.Drawing.Size(230, 21);
+            this.cmbProjetos.TabIndex = 3;
+            this.cmbProjetos.SelectedIndexChanged += new System.EventHandler(this.CmbProjetos_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Projeto:";
+            // 
+            // btnXampp
+            // 
+            this.btnXampp.Location = new System.Drawing.Point(11, 16);
+            this.btnXampp.Name = "btnXampp";
+            this.btnXampp.Size = new System.Drawing.Size(106, 23);
+            this.btnXampp.TabIndex = 1;
+            this.btnXampp.Text = "Localizar Xampp";
+            this.btnXampp.UseVisualStyleBackColor = true;
+            this.btnXampp.Click += new System.EventHandler(this.BtnXampp_Click);
+            // 
+            // txtXampp
+            // 
+            this.txtXampp.Enabled = false;
+            this.txtXampp.Location = new System.Drawing.Point(123, 19);
+            this.txtXampp.Name = "txtXampp";
+            this.txtXampp.Size = new System.Drawing.Size(230, 20);
+            this.txtXampp.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtXampp);
+            this.groupBox1.Controls.Add(this.btnXampp);
+            this.groupBox1.Controls.Add(this.txtProjetos);
+            this.groupBox1.Controls.Add(this.btnProjetos);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 74);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configurações";
+            // 
+            // ProjectSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 160);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbProjetos);
+            this.Controls.Add(this.btnSalvar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ProjectSelector";
+            this.Text = "Troca de Projetos do Apache";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button btnProjetos;
+        private System.Windows.Forms.TextBox txtProjetos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbProjetos;
+        private System.Windows.Forms.TextBox txtXampp;
+        private System.Windows.Forms.Button btnXampp;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ApachePathSwitch/ProjectSelector.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ApachePathSwitch
+{
+    public partial class ProjectSelector : Form
+    {
+        #region Parâmetros
+
+        private string projeto;
+        private string caminhoXampp;
+        private string caminhoProjetos;
+
+        #endregion
+
+        #region Construtor
+
+        public ProjectSelector()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Métodos Privados
+
+        private void AtualizaCampos()
+        {
+            txtXampp.Text = string.IsNullOrWhiteSpace(caminhoXampp) ? string.Empty : caminhoXampp;
+            txtProjetos.Text = string.IsNullOrWhiteSpace(caminhoProjetos) ? string.Empty : caminhoProjetos;
+
+            if (ValidaCampos())
+                btnSalvar.Enabled = true;
+        }
+
+        private void PreencheComboBox()
+        {
+            cmbProjetos.Items.Clear();
+
+            string[] pastas = Directory.GetDirectories(caminhoProjetos);
+
+            foreach (string pasta in pastas)
+                cmbProjetos.Items.Add(RemovePrefixo(pasta));
+        }
+
+        private void SalvaXML()
+        {
+            try
+            {
+                using (DataSet dsResultado = new DataSet())
+                {
+                    if(File.Exists("config.xml"))
+                        dsResultado.ReadXml("config.xml");
+
+                    if (dsResultado.Tables?.Count == 0)
+                    {
+                        XmlTextWriter writer = new XmlTextWriter("config.xml", System.Text.Encoding.UTF8);
+
+                        writer.WriteStartDocument(true);
+                        writer.Formatting = Formatting.Indented;
+                        writer.Indentation = 2;
+
+                        writer.WriteStartElement("Config");
+
+                        writer.WriteStartElement("CaminhoXampp");
+                        writer.WriteString(caminhoXampp);
+                        writer.WriteEndElement();
+
+                        writer.WriteStartElement("CaminhoProjetos");
+                        writer.WriteString(caminhoProjetos);
+                        writer.WriteEndElement();
+
+                        writer.WriteEndDocument();
+
+                        writer.Close();
+                        dsResultado.ReadXml("config.xml");
+                    }
+                    else
+                    {
+                        dsResultado.Tables[0].Rows[dsResultado.Tables[0].Rows.Count - 1]["CaminhoXampp"] = caminhoXampp;
+                        dsResultado.Tables[0].Rows[dsResultado.Tables[0].Rows.Count - 1]["CaminhoProjetos"] = caminhoProjetos;
+                        dsResultado.AcceptChanges();
+
+                        dsResultado.WriteXml("config.xml");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao salvar XML: \n{ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CarregaXML()
+        {
+            try
+            {
+                using (DataSet dsResultado = new DataSet())
+                {
+                    dsResultado.ReadXml("config.xml");
+                    if (dsResultado.Tables.Count != 0)
+                    {
+                        caminhoXampp = dsResultado.Tables[0].Rows[dsResultado.Tables[0].Rows.Count - 1]["CaminhoXampp"].ToString();
+                        caminhoProjetos = dsResultado.Tables[0].Rows[dsResultado.Tables[0].Rows.Count - 1]["CaminhoProjetos"].ToString();
+
+                        AtualizaCampos();
+                        PreencheComboBox();
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void SalvaCaminho()
+        {
+            try
+            {
+                string caminhoConf = $@"{caminhoXampp}\apache\conf\httpd.conf";
+                string caminhoProjeto = $@"{caminhoProjetos}\{projeto}";
+
+                string[] lines = File.ReadAllLines(caminhoConf);
+
+                using (StreamWriter writer = new StreamWriter(caminhoConf))
+                {
+                    foreach (string line in lines)
+                    {
+                        if (line.ToString().StartsWith("DocumentRoot"))
+                        {
+                            writer.WriteLine($"DocumentRoot \"{caminhoProjeto}\"");
+                        }
+                        else if (line.ToString().StartsWith("<Directory"))
+                        {
+                            writer.WriteLine($"<Directory \"{caminhoProjeto}\">");
+                        }
+                        else
+                        {
+                            writer.WriteLine(line.ToString());
+                        }
+                    }
+                }
+
+                MessageBox.Show("Caminho alterado com sucesso");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private bool ValidaCampos() => !string.IsNullOrWhiteSpace(txtXampp.Text)
+                                    && !string.IsNullOrWhiteSpace(txtProjetos.Text)
+                                    && !string.IsNullOrWhiteSpace(cmbProjetos.Text);
+
+        private string SelecionaPasta()
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK)
+                    return fbd.SelectedPath;
+                else
+                    return null;
+            }
+        }
+
+        private string RemovePrefixo(string caminho) => caminho.Replace($@"{caminhoProjetos}\", string.Empty);
+
+        #endregion
+
+        #region Eventos
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregaXML();
+        }
+
+        private void BtnXampp_Click(object sender, EventArgs e)
+        {
+            caminhoXampp = SelecionaPasta();
+
+            AtualizaCampos();
+            PreencheComboBox();
+        }
+
+        private void BtnProjetos_Click(object sender, EventArgs e)
+        {
+            caminhoProjetos = SelecionaPasta();
+
+            AtualizaCampos();
+            PreencheComboBox();
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            SalvaCaminho();
+            SalvaXML();
+        }
+
+        private void CmbProjetos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AtualizaCampos();
+
+            projeto = cmbProjetos.Text;
+        }
+
+        #endregion
+    }
+}
